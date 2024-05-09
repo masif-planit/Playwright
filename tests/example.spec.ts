f9ba1c66856f48759e51128393058f89f734e807
Mohamed Asif <mohamed.asif@specsavers.com>
2024-05-09 23:01:06 +0100

    commit 2

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,26 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.bbc.co.uk/sport');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+ // await expect(page).toHaveTitle(/SPORT/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.bbc.co.uk/sport/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // Click the home  link.
+  await page.locator('xpath=//*[@id="product-navigation-menu"]/div[2]/ul/li[1]/a/span').click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Click the Football link
+  await page.locator('xpath=//*[@id="product-navigation-menu"]/div[2]/ul/li[2]/a/span').click();
+
+ // Click the fixtures
+ await page.locator('xpath=//span[normalize-space()="Scores & Fixtures"]').click();
+
+
+
+ // Click search for team
+ await page.locator('xpath=//input[@id="searchInput"]').click();
 });
